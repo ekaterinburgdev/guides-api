52e1d720e362650ace193b417c1d8b332f9fd98e
OksanaOriflame <emr201000@gmail.com>
2022-08-27 15:40:02 +0500

    PageElement type and PageTreeNode url max length 30 -> 70

--- manuals/app/models.py
+++ manuals/app/models.py
@@ -13,7 +13,7 @@ class PageElement(models.Model):
     id = models.TextField(primary_key=True)
     content = models.JSONField()
     children = models.ManyToManyField("PageElement", symmetrical=False)
-    type = models.CharField(max_length=30)
+    type = models.CharField(max_length=70)
     last_edited = models.DateTimeField(blank=False)
     order = models.IntegerField(db_index=True, default=0)
 
@@ -24,7 +24,7 @@ class PageTreeNode(models.Model):
     child_page = models.ForeignKey(PageElement, null=True, default=None, on_delete=models.CASCADE)
     child_nodes = models.ManyToManyField("PageTreeNode", symmetrical=False)
     last_edited = models.DateTimeField(blank=False)
-    url = models.CharField(max_length=30, null=True, default=None, blank=False, db_index=True)
+    url = models.CharField(max_length=70, null=True, default=None, blank=False, db_index=True)
     show = models.BooleanField(default=True)
 
 
