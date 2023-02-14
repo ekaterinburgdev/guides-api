1f76d5481113c964d618db6111f13c0c7bca2011
OksanaOriflame <emr201000@gmail.com>
2023-02-15 01:41:05 +0500

    bot test

--- manuals/app/internal/infrastructure/db_fill/update_request.py
+++ manuals/app/internal/infrastructure/db_fill/update_request.py
@@ -1,3 +1,4 @@
+from typing import List, Optional, Tuple
 from app.models import PageTreeNode
 
 
@@ -5,3 +6,22 @@ class UpdateRequest:
     def __init__(self, page_tree_node: PageTreeNode = None, force_update: bool = False) -> None:
         self.page_tree_node = page_tree_node
         self.force_update = force_update
+    
+def from_command(args: List[str]) -> Tuple[Optional[UpdateRequest], Optional[str]]:
+    #TODO: options
+    options = list(filter(lambda x: x.startswith("--"), args))
+    force = "--force" in options
+
+    page_ids = list(filter(lambda x: not x.startswith("--"), args))
+    if len(page_ids) == 0:
+        return UpdateRequest(None, force), None
+    
+    if len(page_ids) > 1:
+        return None, f"Я не понимаю, кого из них апдейтить: {', '.join(page_ids)}"
+    
+    page_id = page_ids[0]
+    node = PageTreeNode.objects.filter(id=page_id).first()
+    if not node:
+        return None, f"У меня нет страницы с таким идентификатором: {page_id}"
+    
+    return UpdateRequest(node, force), None
--- manuals/app/tg_bot/manuals_update_handler.py
+++ manuals/app/tg_bot/manuals_update_handler.py
@@ -5,7 +5,7 @@ from telegram.ext import CallbackContext
 
 from django.conf import settings
 
-from app.internal.infrastructure.db_fill import db_update_v4, page_tree_update, prerender_page_elements
+from app.internal.infrastructure.db_fill import db_update_v4, page_tree_update, prerender_page_elements, update_request
 
 
 def force_update_pages(update: Update, context: CallbackContext):
@@ -54,6 +54,34 @@ def update_pages(update: Update, context: CallbackContext):
 
 def test(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
-    message = update.message
-    eff_message = update.effective_message
-    context.bot.send_message(chat_id=chat_id, text=f"yo\n{message}\n{eff_message}")
+    args = update.effective_message.text.split()[1:]
+    if chat_id != int(settings.TG_CHAT_TO_LISTEN) and chat_id != int(settings.TG_MASTER_ID):
+        context.bot.send_message(chat_id=chat_id, text="Я тебе не подчиняюсь, дорогуша...")
+        return
+    if not settings.STATE.available:
+        context.bot.send_message(chat_id=chat_id, text="Да я блин и так пытаюсь, че ты(")
+        return
+
+    request, error_msg = update_request.from_command(args)
+
+    if not request:
+        msg = error_msg if error_msg else "Чета пошло не так((((("
+        context.bot.send_message(chat_id=chat_id, text=msg)
+        return
+
+    msg = "Капец ты жесткий...\nНу ладно, я попытаюсь" if request.force_update else "Окей, сейчас попробую обновить"
+    context.bot.send_message(chat_id=chat_id, text=msg)
+    try:
+        settings.STATE.block()
+        #page_tree_update.update_page_tree()
+        #db_update_v4.check_db(request)
+        #prerender_page_elements.prerender_all()
+        sleep(3)
+        msg = "Прикинь, всё получилось 0_о" if request.force_update else "Вроде обновился"
+        context.bot.send_message(chat_id=chat_id, text=msg)
+    except Exception as e:
+        logging.error(f"[Caching error]; {str(e)}")
+        msg = "Всё потеряно, всё поламалось...\nhttps://godsaves.ru/molitva-o-spasenii-dushi/" if request.force_update else "Чета поломалось"
+        context.bot.send_message(chat_id=chat_id, text=msg)
+    finally:
+        settings.STATE.unblock()
